545f548e74fd3302ecb0589ea40d00dd4deaa3b5
曾宁忠 <zengningzhong@souche.com>
2017-12-02 14:14:04 +0800

    🍰🍰 fast fixs[Fucking submodule bull shit]

--- diceroller-test-parent/diceroller-test-service/src/test/java/com/dicero/diceroller/test/service/EthAddressServiceTest.java
+++ diceroller-test-parent/diceroller-test-service/src/test/java/com/dicero/diceroller/test/service/EthAddressServiceTest.java
@@ -15,6 +15,8 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p></p>
@@ -31,14 +33,17 @@ public class EthAddressServiceTest extends TestBase {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-    private static boolean TEST_V = false;
+    private static boolean TEST_V = true;
     private static String TEST_KEYHASH = "243d9080ee35193511ebc1fab00acdfbeb135d0901bbaa0b5b7c66d3df7ea752d354d8b75cdc7b03d3af916cf3e18f00aaca455526322bf1e0352d12de7e7155";
     private static String TEST_CHAINCODE = "d354d8b75cdc7b03d3af916cf3e18f00aaca455526322bf1e0352d12de7e7155";
     private static String TEST_PUBLIC = "xpub661MyMwAqRbcGeU4wEhPkAW7BL8Z5PL7X3AUxLq2mrYDMBdhLG16AxQx4GdjsfF8gzz2xCZRh8Bm1QSMCAUKeCGt5Tehb1acfRtnYY2XEw7";
     private static String TEST_PRIVATE = "xprv9s21ZrQH143K4APbqDAPP2ZNdJJ4fvcG9pEt9xRRDX1EUPJYnigqdA6UD1Y1pT3TuD1M19QjDgMgZSaqG46sAnMkaeUkcdNTPAmho29hBvu";
 
     private static String passphrase = "crazy horse battery staple";
-    private static String seed = Seed.BITCOIN_SEED;
+    private static String seed = "Bitcoin seed";
+    private static int startIndex = 0;
+    private static int endIndex = 3;
+    private static boolean needSave = true;
 
     private static boolean compressed = true;
     private static String func = "SHA-256";
@@ -66,7 +71,8 @@ public class EthAddressServiceTest extends TestBase {
         System.out.println("private: " + extendedPrivKey);
         if(TEST_V) Assert.assertTrue(TEST_PRIVATE.equals(extendedPrivKey));
 
-        for (int j = 0; j < 3; j++) {
+        List<EthAddressPO> ethAddressPOList = new ArrayList<>();
+        for (int j = startIndex; j < endIndex; j++) {
             String path = "m/" + j;
             Derivation derivation = new Derivation(extendedKey);
             ExtendedKey child = derivation.derive(path);
@@ -79,7 +85,15 @@ public class EthAddressServiceTest extends TestBase {
             System.out.println("address: " + child.getAddress().toString());
             System.out.println("wif: " + child.getWIF());
 
+            ethAddressPOList.add(createEthAddressPO(child.getAddress().toString(), child.serializePublic(), child.serializePrivate()));
         }
+
+        if (needSave) {
+            for (EthAddressPO ethAddressPO : ethAddressPOList) {
+                ethAddressPORepository.save(ethAddressPO);
+            }
+        }
+
     }
 
     private static EthAddressPO createEthAddressPO(String address, String xpub, String xpri){
