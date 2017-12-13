cd50ba91aed8e1df1e58a3faf523fbe160713f08
hibehero <hibehero@gmail.com>
2017-12-13 23:54:37 +0800

    添加提现

--- diceroller-web/src/main/resources/bet/src/component/dialog/Withdrawal.js
+++ diceroller-web/src/main/resources/bet/src/component/dialog/Withdrawal.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import {Modal} from 'antd';
 import './Withdrawal.less';
+import {observer, inject} from "mobx-react";
 
-class Withdrawal extends Component {
+@inject((allStores) => ({
+    withDrawal: allStores.dialogStore.withDrawal
+}))@observer class Withdrawal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            address: '',
+            amt: ''
+        };
+    }
     handleOk = (e) => {
       console.log(e);
       this.props.setWithdrawalVisible(false)
@@ -11,7 +21,17 @@ class Withdrawal extends Component {
       console.log(e);
       this.props.setWithdrawalVisible(false)
     }
+    onChangeAddress = (e) => {
+        this.setState({ address: e.target.value });
+    }
+    onChangeSum = (e) => {
+        if(isNaN(e.target.value)) {
+            return;
+        }
+        this.setState({ amt: e.target.value });
+    }
     render() {
+        const {address, amt} = this.state;
         return(
             <Modal
             title="取款"
@@ -24,15 +44,15 @@ class Withdrawal extends Component {
             <div className="withdrawal">
                <div className="withdrawaCoins">
                    <label>
-                    <span>比特币地址</span>
-                    <input type="type"/>
+                    <span>以太坊地址</span>
+                    <input type="type" value={address} onChange={this.onChangeAddress} />
                    </label>
                    <label>
-                    <span>比特币地址</span>
-                    <input type="type" />
+                    <span>金额 (最小 0.004)</span>
+                    <input type="type" value={amt} onChange={this.onChangeSum} />
                    </label>
                </div>
-               <button>取款</button>
+               <button onClick={this.props.withDrawal.bind(this,address,amt)}>取款</button>
             </div>
           </Modal>
         )
