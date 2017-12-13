a4e46807f878fb86cd366673393cc91234bae095
曾宁忠 <zengningzhong@souche.com>
2017-12-13 23:51:47 +0800

    🍰🍰 fast fixs[Herping the derp]

---
+++ diceroller-admin/src/main/java/com/dicero/diceroller/admin/controller/manager/AdminFundController.java
@@ -0,0 +1,18 @@
+package com.dicero.diceroller.admin.controller.manager;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p></p>
+ *
+ * @author znz
+ * @version 2017/12/13
+ */
+@Slf4j
+@Controller
+@RequestMapping(value="manager/fund")
+public class AdminFundController  {
+
+}
